272a658521887f97093034fceb9d59a18ea65886
Pastito247 <117881893+Pastito247@users.noreply.github.com>
2023-09-07 18:52:03 -0300

    Register

--- MyApp/src/app/register/register.page.ts
+++ MyApp/src/app/register/register.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component,ElementRef, ViewChild, /*Agregado para otras funcionalidades */ OnInit } from '@angular/core';
+//Agregué para animación
+import { Animation, AnimationController, createAnimation } from '@ionic/angular';
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  @ViewChild("title", { read: ElementRef}) title!: ElementRef;
+
+
+  private animation!: Animation;
+
+  constructor(private animationCtrl: AnimationController) {}
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    const cardA = this.animationCtrl
+      .create()
+      .addElement(this.title.nativeElement)
+      .duration(3500)
+      .iterations(Infinity)
+      .keyframes([
+        { offset: 0, transform: 'translateX(0%)' },
+        { offset: 0.5, transform: 'translateX(80%)' },
+        { offset: 1, transform: 'translateX(0%)' },
+      ]);
+      cardA.play();
+  }
 }
