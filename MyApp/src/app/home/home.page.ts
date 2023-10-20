492c539f63d730d06d12952b7ae46e69d1bff49f
1DeGreed <127339535+1DeGreed@users.noreply.github.com>
2023-10-20 08:09:55 -0300

    Add files via upload

--- MyApp/src/app/home/home.page.ts
+++ MyApp/src/app/home/home.page.ts
@@ -1,7 +1,10 @@
 import { Component,ElementRef, ViewChild, /*Agregado para otras funcionalidades */ OnInit } from '@angular/core';
 //Agregué para animación
-import { Animation, AnimationController, createAnimation } from '@ionic/angular';
-  
+import { Animation, AnimationController, IonCard } from '@ionic/angular';
+import { ApiService } from '../api.service';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { EmailService } from '../mail.service';
 
 @Component({
   selector: 'app-home',
@@ -11,14 +14,17 @@ import { Animation, AnimationController, createAnimation } from '@ionic/angular'
 
 export class HomePage implements OnInit {
   @ViewChild("title", { read: ElementRef}) title!: ElementRef;
-
-
+  posts = [];
+  confirmacion = false;
   private animation!: Animation;
-
-  constructor(private animationCtrl: AnimationController) {}
+  correoInput = document.getElementById("Correo");
+  data: any;
+  constructor(private router: Router, private animationCtrl: AnimationController, private api: ApiService
+  , private authService: AuthService, private emailService: EmailService) {}
 
   ngOnInit() {
   }
+
 
   ngAfterViewInit() {
     const cardA = this.animationCtrl
@@ -31,41 +37,73 @@ export class HomePage implements OnInit {
         { offset: 0.5, transform: 'translateX(80%)' },
         { offset: 1, transform: 'translateX(0%)' },
       ]);
-      cardA.play();
-
-
-    const cardB = createAnimation()
-      .addElement(document.getElementsByName('inputs'))
-      
-      .keyframes([
-        { offset: 0, transform: 'translatex(0%)'},
-        { offset: 0.5, transform: 'translatex(100%)' },
-        { offset: 1, transform: 'translatex(0%)' },
-      ]);
-
-
-
-    this.animation = this.animationCtrl
-      .create()
-      .duration(2000)
-      .iterations(1)
-      .addAnimation([cardB]);
-  }
-
-  delete() {
-    this.animation.play();
-  }
-  Email: string = ''; // Inicializa el valor del campo de entrada
-  Contra: string='';
-  clearInput() {
-    this.Email = ''; // Limpia el valor del campo de entrada
-    this.Contra= '';
-  }
+      cardA.play();}
   
   isModalOpen = false;
 
   setOpen(isOpen: boolean) {
     this.isModalOpen = isOpen;
   }
-  
+
+  validar() {
+    var correo = (<HTMLInputElement>document.getElementById("correo")).value;
+    var contra = (<HTMLInputElement>document.getElementById("contra")).value;
+    console.log(correo);
+    
+    // validacion
+    this.api.getPosts(correo).subscribe((res) => {
+      this.confirmacion = true;
+      console.log("Verificando: CORRESTO");
+      this.posts = res["items"];
+      if(res["contrasena"] == contra){
+        const usuario = res["nombre"];
+        const fono = res["telefono"];
+        const conductor = res["conductor"];
+        const direccion = res["direccion"]
+        this.authService.setDatos(correo,usuario,fono,conductor,direccion);
+        this.router.navigate(['/lobby']);
+      }else{
+        console.log("error")
+        this.animation = this.animationCtrl
+        .create()
+        .addElement(document.getElementsByName("Contra"))
+        .duration(500)
+        .iterations(2)
+        .keyframes([
+          { offset: 0,transform: 'translateX(0px)' },
+          { offset: 0.5, transform: 'translateX(10px)' },
+          { offset: 1, transform: 'translateX(0px)' },
+        ])
+        this.animation.play();
+      }
+      
+    }, (error) => {
+      this.confirmacion = false;
+      console.log("Error");
+      this.animation = this.animationCtrl
+      .create()
+      .addElement(document.getElementsByName("Correo"))
+      .duration(500)
+      .iterations(2)
+      .keyframes([
+        { offset: 0,transform: 'translateX(0px)' },
+        { offset: 0.5, transform: 'translateX(10px)' },
+        { offset: 1, transform: 'translateX(0px)' },
+      ])
+      this.animation.play();
+    })
+  }
+ 
+  enviarCorreo() {
+    this.authService.getMail();
+    this.emailService.enviarCorreo('carl.molina@duocuc.cl ', 'Prueba', 'Epico')
+      .subscribe(
+        (response) => {
+          console.log('Correo enviado con éxito', response);
+        },
+        (error) => {
+          console.error('Error al enviar el correo', error);
+        }
+      );
+  }
 }
--- MyApp/src/app/lobby/lobby.page.ts
+++ MyApp/src/app/lobby/lobby.page.ts
@@ -1,46 +1,92 @@
 import { Component, OnInit, ElementRef, ViewChild} from '@angular/core';
-import { GoogleMap, Marker} from "@capacitor/google-maps";
+import { GoogleMap, Marker} from '@capacitor/google-maps';
 import { ModalController } from "@ionic/angular";
 import { environment } from "src/environments/environment";
 import { ModalPage } from "../modal/modal.page";
-import { from } from 'rxjs';
+import { ApiService } from '../api.service';
+import { AuthService } from '../auth.service';
+import { Geolocation} from '@capacitor/geolocation';
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-lobby',
   templateUrl: './lobby.page.html',
   styleUrls: ['./lobby.page.scss'],
 })
-export class LobbyPage {
+export class LobbyPage implements OnInit {
   @ViewChild('map')mapRef!: ElementRef;
   map!: GoogleMap;
+  posts = [];
+  usuario!: string | null;
+  correo!: string | null;
+  fono!: number | null;
+  conductor!: number | null;
+  direccion!: string | null;
 
-  constructor(private modalCtrl:ModalController) {}
+  isConductor: boolean = false;
+  constructor(private modalCtrl:ModalController, private api: ApiService,private authService: AuthService) {}
 
+  ngOnInit() {
+    this.usuario = this.authService.getUser();
+    this.fono = this.authService.getFono();
+    this.correo = this.authService.getMail();
+    this.direccion = this.authService.getDireccion();
+    this.conductor = this.authService.getConductor();
+
+    if(this.conductor === 1){
+      this.isConductor = true;
+    }
+
+    console.log(this.conductor)
+  }
 
   ionViewDidEnter(){
     this.createMap();
   }
 
 
-  async createMap(){
+  async createMap() {
+    const coordinates = await Geolocation.getCurrentPosition();
+    const lat = coordinates.coords.latitude;
+    const lng = coordinates.coords.longitude;
+  
     this.map = await GoogleMap.create({
-    id: 'my-map',
-    apiKey: environment.mapsKey,
-    element: this.mapRef.nativeElement,
-    config: {
-      center: {
-        lat:-33.04353334085918,
-        lng:-71.60435289558721,
-
-      },
-      zoom: 14,
-    }
+      id: 'my-map',
+      apiKey: environment.mapsKey,
+      element: this.mapRef.nativeElement,
+      config: {
+        center: {
+          lat: lat,
+          lng: lng,
+        },
+        zoom: 14,
+      }
     });
+  
     await this.addMarkers();
-    
   }
+  
 
+<<<<<<< HEAD
+  async addMarkers() {
+    const coordinates = await Geolocation.getCurrentPosition();
+    const lat = coordinates.coords.latitude;
+    const lng = coordinates.coords.longitude;
+  
+    const markers: Marker[] = [
+      {
+        coordinate: {
+          lat: lat,
+          lng: lng,
+        },
+        title: 'Su ubicación',
+        snippet: 'Estás aquí',
+      }
+=======
   async addMarkers(){
+    
     const markers: Marker[] =[
     {
       coordinate:{
@@ -59,26 +105,25 @@ export class LobbyPage {
       snippet:'not sure',
       },
     
+>>>>>>> parent of 0b2f1f0e (cambios)
     ];
-
+  
     const result = await this.map.addMarkers(markers);
-   
-
-    this.map.setOnMarkerClickListener(async (marker) =>{
+  
+    this.map.setOnMarkerClickListener(async (marker) => {
       const modal = await this.modalCtrl.create({
         component: ModalPage,
         componentProps: {
-        marker,},
-        breakpoints:[0, 0.3],
+          marker,
+        },
+        breakpoints: [0, 0.3],
         initialBreakpoint: 0.3,
-       
-      
       });
       modal.present();
-
-
     });
   }
-}
+  
+  }
+
   
 
